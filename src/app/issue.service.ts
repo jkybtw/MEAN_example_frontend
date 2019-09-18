fb9f7190aa7400b2b5c469febc1ca58802582c1e
Jacky <liangj97@gmail.com>
2019-09-18 20:04:10 +1000

    Update backend endpoint

--- src/app/issue.service.ts
+++ src/app/issue.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class IssueService {
 
-  uri = 'https://stormy-ridge-10701.herokuapp.com';
+  uri = 'https://powerful-everglades-94556.herokuapp.com';
   // creates instance of http in class to use
   constructor(private http: HttpClient) { }
 
